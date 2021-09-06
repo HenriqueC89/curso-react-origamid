3f0e6687488f556adb4ae914b46b1b8b2b5446f9
HenriqueC89 <henriqueconceicao@furg.br>
2021-09-05 23:06:47 -0300

    Aulas 0827-0837: Finalizando curso

---
+++ dogs/src/Components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from './Helper/Head';
+
+const NotFound = () => {
+  return (
+    <section className='container mainContainer'>
+      <Head title='Página não encontrada' />
+      <h1 className='title'>Erro: 404</h1>
+      <p>Página não encontrada.</p>
+    </section>
+  );
+};
+
+export default NotFound;
